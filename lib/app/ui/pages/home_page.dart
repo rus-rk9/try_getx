c1677073461b096827224acf46ed503b61907be0
rusrk9 <rus.rk9@gmail.com>
2021-06-04 01:21:35 +0600

    end, to-do: handle sliverbox moving

--- lib/app/ui/pages/home_page.dart
+++ lib/app/ui/pages/home_page.dart
@@ -1,64 +1,68 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_group_sliver/flutter_group_sliver.dart';
+import 'package:flutter/rendering.dart';
+// import 'package:flutter_group_sliver/flutter_group_sliver.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:try_getx/app/controllers/home_page_controller.dart';
 import 'package:try_getx/app/ui/global_widgets/rounded_long_container.dart';
+import 'dart:math' as Math;
 
 class HomePage extends GetView<HomePageController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: CustomScrollView(
-          slivers: [
-            SliverAppBar(
-              backgroundColor: Colors.transparent,
-              expandedHeight: 200.0,
-              flexibleSpace: /* const */ FlexibleSpaceBar(
-                background: SvgPicture.asset(
-                  'lib/assets/circle.svg',
-                  alignment: Alignment.centerLeft,
-                ),
-              ),
-            ),
-            SliverGroupBuilder(
-              decoration: BoxDecoration(
-                color: Colors.red,
-                borderRadius: BorderRadius.all(Radius.circular(16)),
-                // border: Border.all(
-                //   color: Color.fromRGBO(238, 237, 238, 1),
-                // ),
-              ),
-              child: SliverList(
-                delegate: SliverChildBuilderDelegate(
-                  (BuildContext context, int index) {
-                    return Container(
-                      // margin: EdgeInsets.only(top: 64),
-                      padding: EdgeInsets.only(top: 32, left: 24, right: 24, bottom: 3072),
-                      decoration: BoxDecoration(
-                        color: Colors.grey[350],
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(24),
-                          topRight: Radius.circular(24),
+        child: Container(
+          color: Colors.transparent,
+          child: CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                backgroundColor: Colors.transparent,
+                // collapsedHeight: 1,
+                expandedHeight: 300.0,
+                toolbarHeight: 0,
+                flexibleSpace: Stack(
+                  children: [
+                    Positioned(
+                      left: -120,
+                      top: -98,
+                      child: Transform.rotate(
+                        angle: (-Math.pi / 180) * 0,
+                        alignment: Alignment.center,
+                        child: Container(
+                          width: 300,
+                          height: 300,
+                          child: SvgPicture.asset(
+                            'lib/assets/circle.svg',
+                          ),
                         ),
                       ),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          RoundedLongContainer(width: context.width * 0.6),
-                          RoundedLongContainer(width: context.width * 0.75),
-                          RoundedLongContainer(width: context.width * 0.5),
-                        ],
-                      ),
-                    );
-                  },
-                  childCount: 1,
+                    ),
+                  ],
                 ),
               ),
-            ),
-          ],
+              SliverToBoxAdapter(
+                child: Container(
+                  padding: EdgeInsets.only(top: 32, left: 24, right: 24, bottom: 3072),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[350],
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(24),
+                      topRight: Radius.circular(24),
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      RoundedLongContainer(width: context.width * 0.6),
+                      RoundedLongContainer(width: context.width * 0.75),
+                      RoundedLongContainer(width: context.width * 0.5),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
