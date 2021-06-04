3bbe435d0408ea2e7ffaf6bab5ec37c1310e679b
rusrk9 <rus.rk9@gmail.com>
2021-06-04 11:46:50 +0600

    start

--- lib/app/ui/pages/home_page.dart
+++ lib/app/ui/pages/home_page.dart
@@ -13,53 +13,61 @@ class HomePage extends GetView<HomePageController> {
       body: SafeArea(
         child: Container(
           color: Colors.transparent,
-          child: CustomScrollView(
-            slivers: [
-              SliverAppBar(
-                backgroundColor: Colors.transparent,
-                expandedHeight: 300.0,
-                toolbarHeight: 0,
-                flexibleSpace: Stack(
-                  children: [
-                    Positioned(
-                      left: -120,
-                      top: -98,
-                      child: Obx(() => Transform.rotate(
-                            angle: (-Math.pi / 180) * controller.circleAngle,
-                            alignment: Alignment.center,
-                            child: Container(
-                              width: 300,
-                              height: 300,
-                              child: SvgPicture.asset(
-                                'lib/assets/circle.svg',
-                              ),
-                            ),
-                          )),
-                    ),
-                  ],
-                ),
-              ),
-              SliverToBoxAdapter(
-                child: Container(
-                  padding: EdgeInsets.only(top: 32, left: 24, right: 24, bottom: 3072),
-                  decoration: BoxDecoration(
-                    color: Colors.grey[350],
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(24),
-                      topRight: Radius.circular(24),
-                    ),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+          child: NotificationListener(
+            child: CustomScrollView(
+              slivers: [
+                SliverAppBar(
+                  backgroundColor: Colors.transparent,
+                  expandedHeight: 300.0,
+                  toolbarHeight: 0,
+                  flexibleSpace: Stack(
                     children: [
-                      RoundedLongContainer(width: context.width * 0.6),
-                      RoundedLongContainer(width: context.width * 0.75),
-                      RoundedLongContainer(width: context.width * 0.5),
+                      Positioned(
+                        left: -120,
+                        top: -98,
+                        child: Obx(() => Transform.rotate(
+                              angle: (-Math.pi / 180) * controller.circleAngle,
+                              alignment: Alignment.center,
+                              child: Container(
+                                width: 300,
+                                height: 300,
+                                child: SvgPicture.asset(
+                                  'lib/assets/circle.svg',
+                                ),
+                              ),
+                            )),
+                      ),
                     ],
                   ),
                 ),
-              ),
-            ],
+                SliverToBoxAdapter(
+                  child: Container(
+                    padding: EdgeInsets.only(top: 32, left: 24, right: 24, bottom: 3072),
+                    decoration: BoxDecoration(
+                      color: Colors.grey[350],
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(24),
+                        topRight: Radius.circular(24),
+                      ),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        RoundedLongContainer(width: context.width * 0.6),
+                        RoundedLongContainer(width: context.width * 0.75),
+                        RoundedLongContainer(width: context.width * 0.5),
+                      ],
+                    ),
+                  ),
+                ),
+
+                // ),
+              ],
+            ),
+            onNotification: (notification) {
+              print('nofification: ${notification.toString()}');
+              return true;
+            },
           ),
         ),
       ),
